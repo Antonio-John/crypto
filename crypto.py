2387b1b5d57f22563a30ecf67009ac21c733ed8f
pyantonio-coder <antonio.felton@virginmedia.com>
2021-10-31 18:55:11 +0000

    adding atbash

--- crypto.py
+++ crypto.py
@@ -1,13 +1,26 @@
-from tools import (type_str_error, type_alphabet_error,get_pos_alphabet)
+from tools import (type_str_error, 
+                   type_alphabet_error,
+                   get_pos_alphabet,
+                   get_atbashed_pos_alphabet,
+                   alphabet_dictionary,
+                   alphabet,
+                   get_alphabet_from_pos)
 
 def atbash_decrypt(cipher_text):
 
-    type_str_error(cipherText)
-    type_alphabet_error(cipherText)
+    type_str_error(cipher_text)
+    type_alphabet_error(cipher_text)
     
     
-    letter_pos=get_position_alphabet(cipher_text)
+    letter_pos=get_pos_alphabet(cipher_text)
+    atbashed_pos=get_atbashed_pos_alphabet(letter_pos)
+    plain_text=get_alphabet_from_pos(atbashed_pos)
     
-    return
+    plain_text_joined = "".join(plain_text)
+    
+    
+    return plain_text_joined
+
+
 
 
