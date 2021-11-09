d468425eb4435fc6cd86f5a6b74705fcf74dfadb
Antonio Felon <antonio.felton@ons.gov.uk>
2021-11-09 08:14:40 +0000

    fixing conflict for test crypto

--- test_crypto.py
+++ test_crypto.py
@@ -1,7 +1,4 @@
 #print(ceaser_decrpyt("EZOLJ", shift=11)) should be equal to today
 
 #get_ceaser_pos_decrypt(letters_pos,shift)
-
-#test=vigenere_decrypt(cipher_text="FGQVEQYONMCCHAXTPB", 
-#                      keyword="FAMILY")   
-#AGENTSTOBERECALLED
+#print(ceaser_encrypt("TODAY", 11))
