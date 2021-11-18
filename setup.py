165404e6e7064c7df3c223cd74b5dc5db3a5ce7f
Antonio Felon <antonio.felton@ons.gov.uk>
2021-11-18 08:50:58 +0000

    set up, need to look into innit files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='cryptology',
+      version='1.0',
+      description='Decrypt/Encrypt text using various cipher techniques',
+      author='Antonio Felton',
+      author_email='',
+      url='',
+      packages_name=['cryptology'],
+     )
