cee216c10bc5a188bf4aa877618d8fb679082569
pyantonio-coder <antonio.felton@virginmedia.com>
2021-12-01 22:19:31 +0000

    another version

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md","r") as fh:
       long_description=fh.read()
 
 setup(name='cryptology',
-      version='0.0.8',
+      version='0.0.9',
       description='Decrypt/Encrypt text using various cipher techniques',
       long_description=long_description,
       long_description_content_type="text/markdown",
